2bae64ecf038fa04a16620927cae6c25c046950a
Zormelo-Alex <alexzormelo9@gmail.com>
2023-08-24 12:01:05 +0100

    started the api trials

--- Weather-Api/wApi.py
+++ Weather-Api/wApi.py
@@ -3,31 +3,31 @@ from pyowm.utils import config
 from pyowm.utils import timestamps
 
 # ---------- FREE API KEY examples ---------------------
-owm = OWM('1d0236ee9c32c60ef15d05abf06a479f')
+owm = OWM('9feb8ab55ac666276d6eed156cbec7eb')
 mgr = owm.weather_manager()
 
+place = input("Please enter a city\n")
 
+place = place.lower()
 # Search for current weather in London (Great Britain) and get details
-observation = mgr.weather_at_place('London,GB')
+#observation = mgr.weather_at_place('London,GB')
+observation = mgr.weather_at_place(place)
+
 w = observation.weather
 
-w.detailed_status         # 'clouds'
-w.wind()                  # {'speed': 4.6, 'deg': 330}
-w.humidity                # 87
-w.temperature('celsius')  # {'temp_max': 10.5, 'temp': 9.7, 'temp_min': 9.0}
-w.rain                    # {}
-w.heat_index              # None
-w.clouds                  # 75
+detailed = w.detailed_status         # 'clouds'
+wind = w.wind()                  # {'speed': 4.6, 'deg': 330}
+humidity = w.humidity                # 87
+temperature = w.temperature('celsius')  # {'temp_max': 10.5, 'temp': 9.7, 'temp_min': 9.0}
+rain = w.rain                    # {}
+heat = w.heat_index              # None
+clouds = w.clouds 
 
-# Will it be clear tomorrow at this time in Milan (Italy) ?
-forecast = mgr.forecast_at_place('Milan,IT', 'daily')
-answer = forecast.will_be_clear_at(timestamps.tomorrow())
+if(place == "hong kong"):
+    print(f"The humidity in {place.upper()} is {humidity}")
+elif(place == "tokyo"):
+    print(f"The temperature in {place.upper()} is {temperature['temp']} degrees celcius")
+else:
+    print(f"In {place.upper()} there's, {detailed} \nthe speed of wind is {wind['speed']} \nthe humidity is {humidity} \nthe temperature is {temperature['temp']} degrees celcius \nrain is {rain} \nheat is {heat} \nand cloud is {clouds}")
 
-# ---------- PAID API KEY example ---------------------
 
-config_dict = config.get_default_config_for_subscription_type('professional')
-owm = OWM('your paid OWM API key', config_dict)
-
-# What's the current humidity in Berlin (Germany) ?
-one_call_object = mgr.one_call(lat=52.5244, lon=13.4105)
-one_call_object.current.humidity
