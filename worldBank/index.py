b9a6b18b2b07a3a4a9442e6b386a49cbc401cc6b
Zormelo-Alex <alexzormelo9@gmail.com>
2023-08-25 16:39:20 +0100

    started working with the breadboard!

--- worldBank/index.py
+++ worldBank/index.py
@@ -1,6 +1,32 @@
 import wbdata
 
 source = wbdata.get_source()
-country_info = wbdata.search_countries('united')
 
-print(country_info)
+country = input("Please enter the country\n")
+
+country_info = wbdata.search_countries(country)
+
+indicator = wbdata.get_indicator(source=1)
+
+def getCode():
+    for i in country_info:
+        return i['id']
+
+code = getCode()
+
+country_name = ''
+
+for i in indicator:
+    country_details = wbdata.get_data(i['id'], country = code)
+    date = country_details[0]['date']
+
+    for j in country_details:
+        print(f"{j} \n\n")
+    
+# for j in country_details:
+#     # print(j)
+#     country_name = j['country']['value']
+
+# print(country_name)
+# print(date)
+# print(indicator)
